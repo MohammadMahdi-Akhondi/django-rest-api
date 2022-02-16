1dc731e6ecfabc7ff8a77fb1b817c39bed954e8d
MohammadMahdi-Akhondi <mohammad.a8098@gmail.com>
2022-02-16 15:01:11 +0330

    Create and register book model

--- api/models.py
+++ api/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Book(models.Model):
-    title  = models.CharField(max_length = 150)
-    author = models.ForeignKey(User, on_delete = models.CASCADE)
-    page_count = models.IntegerField()
+    title        = models.CharField(max_length = 150)
+    author       = models.ForeignKey(User, on_delete = models.CASCADE)
+    page_count   = models.IntegerField()
     is_available = models.BooleanField()
 
     def __str__(self):
