a3a73b847bb3e950a523bbf33bd4227375296802
MohammadMahdi-Akhondi <mohammad.a8098@gmail.com>
2022-02-16 14:58:30 +0330

    Create and register book model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Book(models.Model):
+    title  = models.CharField(max_length = 150)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    page_count = models.IntegerField()
+    is_available = models.BooleanField()
+
+    def __str__(self):
+        return self.title
